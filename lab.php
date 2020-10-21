ea0c95649de397391f43577c1d6d99c41461de0c
sitmalidis <sitmalidi@yahoo.com>
2020-10-22 00:21:54 +0300

    Interval INPUT FORMAT

--- lab.php
+++ lab.php
@@ -33,20 +33,26 @@ and open the template in the editor.
             <tbody>
                 <tr>
                     <td><input id="timeCheckBox" type="checkbox" onclick="checkTimeInput()"></td>
-                    <td >ბრუნის დრო</td><td>საათი<br><input id="timeInputHour" type="number" min="0" disabled="true" value="02" onchange="calculateByTime()" onkeyup="calculateByTime()"></td><td>წუთი<br><input id="timeInputMinute" type="number" disabled="true" value="00" onchange="calculateByTime()" onkeyup="calculateByTime()"></td><td>წამი<br><input id="timeInputSecond" type="number" disabled="true" value="00" onchange="calculateByTime()" onkeyup="calculateByTime()"></td>
+                    <td >ბრუნის დრო</td><td>საათი<br><input id="timeInputHour" type="number" disabled="true" max="24" min="0" value="02"  oninput="calculateByTime()"></td><td>წუთი<br><input id="timeInputMinute" type="number" disabled="true" max="59" min="0" value="00" onchange="calculateByTime()" onkeyup="calculateByTime()"></td><td>წამი<br><input id="timeInputSecond" type="number" disabled="true" max="59" min="0" value="00" onchange="calculateByTime()" onkeyup="calculateByTime()"></td>
 
                 </tr>
                 <tr>
                     <td><input id="busCheckBox" type="checkbox" onclick="checkBusInput()"></td>
-                    <td>ავტობუსების<br> რაოდენობა</td><td colspan="3"><input id="busInput" type="number" disabled="true" style="width:125px;" max="200" min="1" value="1" oninput="calculateByBus()"></td>
+                    <td>ავტობუსების<br> რაოდენობა</td><td colspan="3"><input id="busInput" type="number" disabled="true" style="width:125px;" max="200" min="1" value="1" onchange="calculateByBus()" onkeyup="calculateByBus()"></td>
                 </tr>
                 <tr>
                     <td><input id="intervalCheckBox" type="checkbox" onclick="checkIntervalInput()"></td>
-                    <td>ინტერვალი</td><td>საათი<br><input id="intervalInputHour" type="number" disabled="true" value="00" onchange="calculateByInterval()" onkeyup="calculateByInterval()"></td><td>წუთი<br><input id="intervalInputMinute" type="number" disabled="true" value="10" onchange="calculateByInterval()" onkeyup="calculateByInterval()"></td><td>წამი<br><input id="intervalInputSecond" type="number" disabled="true"  value="00" onchange="calculateByInterval()" onkeyup="calculateByInterval()"></td>
+                    <td>ინტერვალი</td><td>საათი<br><input id="intervalInputHour" type="number" disabled="true" max="24" min="0" value="00" onchange="calculateByInterval()" onkeyup="calculateByInterval()"></td><td>წუთი<br><input id="intervalInputMinute" type="number" disabled="true" max="59" min="0" value="10" onchange="calculateByInterval()" onkeyup="calculateByInterval()"></td><td>წამი<br><input id="intervalInputSecond" type="number" disabled="true" max="59" min="0" value="00" onchange="calculateByInterval()" onkeyup="calculateByInterval()"></td>
                 </tr>
             </tbody>
         </table>
         <label id="notes"></label>
+        <hr>
+        <p>Write something in the text field to trigger a function.</p>
+
+        <input type="number" id="myInput" max="60" oninput="myFunction()">
+
+        <p id="demo"></p>
         <hr>
 
         <script>
@@ -116,32 +122,10 @@ and open the template in the editor.
 
 
             function   calculateByTime() {
-                //plus plus and minus minus result
-                if (timeInputSecond.value == 60) {
-                    timeInputSecond.value = 0;
-                    timeInputMinute.value = parseInt(timeInputMinute.value) + 1;
-                }
-                if (timeInputSecond.value == -1) {
-                    timeInputSecond.value = 59;
-                    timeInputMinute.value = parseInt(timeInputMinute.value) - 1;
-                }
-
-
-                if (timeInputMinute.value == 60) {
-                    timeInputMinute.value = 0;
-                    timeInputHourPlusPlus();
-                }
-                if (timeInputMinute.value == -1) {
-                    timeInputMinute.value = 59;
-                    timeInputHourMinusMinus();
-                }
-
-                //---------------
                 checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
                 if (checkboxes.length < 2) {
                     alert("არჩეულია არასაკარისი პარამეტრები. საჭიროა 2 პარამეტრის არჩევა");
                 } else {
-
                     var seconds = (timeInputHour.value * 60 * 60) + (timeInputMinute.value * 60) + parseInt(timeInputSecond.value);
 
                     if (busCheckBox.checked === true) {
@@ -168,28 +152,6 @@ and open the template in the editor.
 
             }
             function calculateByInterval() {
-                //plus plus and minus minus result
-                if (intervalInputSecond.value == 60) {
-                    intervalInputSecond.value = 0;
-                    intervalInputMinute.value = parseInt(intervalInputMinute.value) + 1;
-                }
-                if (intervalInputSecond.value == -1) {
-                    intervalInputSecond.value = 59;
-                    intervalInputMinute.value = parseInt(intervalInputMinute.value) - 1;
-                }
-
-
-                if (intervalInputMinute.value == 60) {
-                    intervalInputMinute.value = 0;
-                    intervalInputHourPlusPlus();
-                }
-                if (intervalInputMinute.value == -1) {
-                    intervalInputMinute.value = 59;
-                    intervalInputHourMinusMinus();
-                }
-
-                //---------------
-
                 checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
                 if (checkboxes.length < 2) {
                     alert("არჩეულია არასაკარისი პარამეტრები. საჭიროა 2 პარამეტრის არჩევა");
@@ -273,110 +235,20 @@ and open the template in the editor.
             }
 
 
-            function   timeInputHourPlusPlus() {
-                var x = parseInt(timeInputHour.value) + 1
-                if (x < 10) {
-                    timeInputHour.value = "0" + x;
-                } else {
-                    timeInputHour.value = x;
-                }
-            }
 
-            function   timeInputHourMinusMinus() {
-                var x = parseInt(timeInputHour.value) - 1
-                if (x < 0) {
-                    x = 0;
-                    timeInputMinute.value = 0;
-                    alert("საათების 0 ზე ქვემოთ ჩამოსვლა დაუშვებელია");
-
-                }
-                if (x < 10) {
-                    timeInputHour.value = "0" + x;
-                } else {
-                    timeInputHour.value = x;
-                }
-            }
-
-            function   intervalInputHourPlusPlus() {
-                var x = parseInt(intervalInputHour.value) + 1
-                if (x < 10) {
-                    intervalInputHour.value = "0" + x;
-                } else {
-                    intervalInputHour.value = x;
-                }
-            }
-
-            function   intervalInputHourMinusMinus() {
-                var x = parseInt(intervalInputHour.value) - 1
-                if (x < 0) {
-                    x = 0;
-                    intervalInputMinute.value = 0;
-                    alert("საათების 0 ზე ქვემოთ ჩამოსვლა დაუშვებელია");
-
-                }
-                if (x < 10) {
-                    intervalInputHour.value = "0" + x;
-                } else {
-                    intervalInputHour.value = x;
-                }
-            }
 
             //-----------------------------------------
-            function koko() {
-                alert("koko");
-            }
-            var x = 0;
             function myFunction() {
-                var h = document.getElementById("h");
-                var m = document.getElementById("m");
-                var s = document.getElementById("s");
-                var b = document.getElementById("b");
-                var r = document.getElementById("r");
-                var n = document.getElementById("n");
-                var g = document.getElementById("g");
-                var d = document.getElementById("d");
-                if (h.value < 0) {
-                    alert("საათებში მითითებულია დაუშვებელი რიცხვი");
-                    h.value = 0;
+                var x = document.getElementById("myInput").value;
+                if (x ==60) {
+                    document.getElementById("myInput").value = 0;
+                    x=0;
                 }
-                if (m.value < 0 || m.value > 59) {
-                    alert("წუთებში მითითებულია დაუშვებელი რიცხვი");
-                    m.value = 0;
+                if (x ==-1) {
+                    document.getElementById("myInput").value = 59;
+                    x=59;
                 }
-                if (s.value < 0 || s.value > 59) {
-                    alert("წამებში მითითებულია დაუშვებელი რიცხვი");
-                    s.value = 0;
-                }
-                if (b.value <= 0) {
-                    alert("ავტობუსების რაოდენობად მითითებულია დაუშვებელი რიცხვი");
-                    s.value = 1;
-                }
-                var seconds = (h.value * 60 * 60) + (m.value * 60) + parseInt(s.value);
-                var result = seconds / b.value;
-                var date = new Date(0);
-                date.setSeconds(result);
-                var re = date.toISOString().substr(11, 8);
-
-                var na = result % 3600 % 60 % 1;
-
-                r.innerHTML = re;
-                n.innerHTML = na;
-
-
-                var tt = re.split(":");
-                var sec = tt[0] * 3600 + tt[1] * 60 + tt[2] * 1;
-                var newRes = sec * b.value;
-                var date = new Date(0);
-                date.setSeconds(newRes);
-                var newResult = date.toISOString().substr(11, 8);
-                g.innerHTML = newResult;
-
-                var diff = seconds - newRes;
-                var differ = new Date(0);
-                differ.setSeconds(diff);
-                var differResult = differ.toISOString().substr(11, 8);
-                d.innerHTML = differResult;
-
+                document.getElementById("demo").innerHTML = "You wrote: " + x;
             }
         </script>
     </body>
